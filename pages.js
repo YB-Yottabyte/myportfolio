5803bdf9ed39d78915b56ca3d3888333615cb10d
Sai Rithwik <138318585+YB-Yottabyte@users.noreply.github.com>
2024-03-16 16:39:14 -0700

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/YB-Yottabyte/YB-Yottabyte.github.io.git";
+const repoURL = " https://yb-yottabyte.github.io/myportfolio/";
 
 ghpages.publish(
   pathname,
